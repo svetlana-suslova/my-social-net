dcee0914cbfe8d7226be8a9a9fdda3f97b03c7f1
bonial_virto <svetlana.suslova2031@gmail.com>
2020-05-03 15:36:32 +0200

    the very basic components structure and styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import './Header.sass';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK__0dZPtkT7Opgx6qdPJa55dDyMpNj12UwAtzX6LjFrLatc-b&usqp=CAU" alt="logo"/>
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import './Profile.sass';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+        <img src="https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg" alt="brain"/>
+        </div>
+        <div>
+          ava+descr
+        </div>
+        <div>
+          My posts
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>  
+    );
+}
+export default Profile;

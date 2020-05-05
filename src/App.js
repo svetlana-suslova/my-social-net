532a629182c3f5196ea90614dee7d5f07bc7c3a8
bonial_virto <svetlana.suslova2031@gmail.com>
2020-05-05 14:56:59 +0200

    added routing to Navbar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './App.sass';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
-import Dialogs from './components/dialogs/Dialogs';
 import Profile from './components/profile/Profile';
+import Messages from './components/messages/Messages';
+import News from './components/news/News';
+import Music from './components/music/Music';
+import Settings from './components/settings/Settings';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App = () =>  {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
-        <Profile />
+    <Router>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/messages" component={Messages}/>
+          <Route path="/news" component={News}/>
+          <Route path="/music" component={Music}/>
+          <Route path="/settings" component={Settings}/>
+        </div>
       </div>
-    </div>
+    </Router> 
   );
 }
 export default App;
---
+++ src/components/music/Music.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Music.module.sass';
+
+
+const Music = () => {
+    return (
+      <div>
+        Music
+      </div>  
+    );
+}
+export default Music;

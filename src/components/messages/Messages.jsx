956df8900d17929369acda80794fa69abce927c9
bonial_virto <svetlana.suslova2031@gmail.com>
2020-05-07 20:51:05 +0200

    put the data aside

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -19,20 +19,35 @@ const MessagesItem = ({text}) => {
 }
 
 const Messages = () => {
+
+  let dialogsData = [
+    {id: 1, name: 'Argon'},
+    {id: 2, name: 'Butan'},
+    {id: 3, name: 'Neon'},
+    {id: 4, name: 'Crypton'},
+    {id: 5, name: 'Helium'},
+    {id: 6, name: 'Oxygen'}
+  ];
+  let messagesData = [
+    {id: 1, text: 'Hello people.'},
+    {id: 2, text: 'Learning Rect??'},
+    {id: 3, text: 'Viel Spass!'}
+  ];
+
     return (
       <div className={s.content}>
         <div className={s.dialogs}>
-          <DialogsItem name="Argon" id="1"/>
-          <DialogsItem name="Butan" id="2"/>
-          <DialogsItem name="Neon" id="3"/>
-          <DialogsItem name="Crypton" id="4"/>
-          <DialogsItem name="Helium" id="5"/>
-          <DialogsItem name="Oxygen" id="6"/>
+          <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+          <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+          <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+          <DialogsItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+          <DialogsItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+          <DialogsItem name={dialogsData[5].name} id={dialogsData[5].id}/>
         </div>
         <div className={s.messages}>
-          <MessagesItem text="Hello people."/>
-          <MessagesItem text="Learning Rect??"/>
-          <MessagesItem text="Viel Spass!"/>
+          <MessagesItem text={messagesData[0].text} id={messagesData[0].id}/>
+          <MessagesItem text={messagesData[1].text} id={messagesData[1].id}/>
+          <MessagesItem text={messagesData[2].text} id={messagesData[2].id}/>
         </div>
       </div>  
     );
--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -3,22 +3,28 @@ import Post from './post/Post';
 import s from './MyPosts.module.sass';
 
 const MyPosts = () => {
-    return (
-      <div className={s.content}>
-        <h3>My posts</h3>
+
+  let postsData = [
+    {id: 1, text: 'Hi there!', like: 5},
+    {id: 2, text: 'How are you?', like: 12}
+  ];
+
+  return (
+    <div className={s.content}>
+      <h3>My posts</h3>
+      <div>
         <div>
-          <div>
-            <textarea></textarea>
-          </div>
-          <div>
-            <button>Add post</button>
-          </div>
+          <textarea></textarea>
         </div>
-        <div className={s.posts}>
-          <Post message="Hi there!" likes='5'/> 
-          <Post message="How are you?" likes='12'/>
-        </div>  
+        <div>
+          <button>Add post</button>
+        </div>
       </div>
-    );
+      <div className={s.posts}>
+        <Post message={postsData[0].text} likes={postsData[0].like} id={postsData[0].id}/> 
+        <Post message={postsData[1].text} likes={postsData[1].like} id={postsData[1].id}/>
+      </div>  
+    </div>
+  );
 }
 export default MyPosts;

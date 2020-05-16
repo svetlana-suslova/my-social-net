42b8ad905cd12129137ec6fd4d5d4adb2d39c9a9
bonial_virto <svetlana.suslova2031@gmail.com>
2020-05-16 21:55:50 +0200

    refactored state.js as an object

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
 import App from './App';
-import state, {addPost, updateNewPostText, subscribe} from './redux/state';
+import store from './redux/state';
 
 let renderEntireTree = (state) => {
   ReactDOM.render(
-    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+    <App state={state} addPost={store.addPost.bind(store)} 
+    updateNewPostText={store.updateNewPostText.bind(store)}/>,
     document.getElementById('root')
   );
 }
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,47 +1,50 @@
-let renderEntireTree = () => {
-}
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, text: 'Hi there!', like: 5},
-            {id: 2, text: 'How are you?', like: 12}
-        ],
-        newPostText: 'Kamasutra'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, text: 'Hi there!', like: 5},
+                {id: 2, text: 'How are you?', like: 12}
+            ],
+            newPostText: 'Kamasutra'
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Argon'},
+                {id: 2, name: 'Butan'},
+                {id: 3, name: 'Neon'},
+                {id: 4, name: 'Crypton'},
+                {id: 5, name: 'Helium'},
+                {id: 6, name: 'Oxygen'}
+            ],
+            messages: [
+                {id: 1, text: 'Hello people.'},
+                {id: 2, text: 'Learning Rect??'},
+                {id: 3, text: 'Viel Spass!'}
+            ]
+        }   
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Argon'},
-            {id: 2, name: 'Butan'},
-            {id: 3, name: 'Neon'},
-            {id: 4, name: 'Crypton'},
-            {id: 5, name: 'Helium'},
-            {id: 6, name: 'Oxygen'}
-        ],
-        messages: [
-            {id: 1, text: 'Hello people.'},
-            {id: 2, text: 'Learning Rect??'},
-            {id: 3, text: 'Viel Spass!'}
-        ]
-    }   
-}
+    getState() {
+        return this._state
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        text: state.profilePage.newPostText,
-        like: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
+    },
+    _callSubcriber() {
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            text: this._state.profilePage.newPostText,
+            like: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubcriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubcriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubcriber = observer;
+    }
 }
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export const subscribe = (observer)  => {
-    renderEntireTree = observer;
-}
-
-export default state;
+export default store;

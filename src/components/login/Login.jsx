b80a00697f6d51f7e891d7688347565fd71f1a84
bonial_virto <svetlana.suslova2031@gmail.com>
2020-06-11 23:51:19 +0200

    added redux-form and created login  form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form'
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field type="text" name="login" placeholder="Login" component="input"/>
+            </div>
+            <div>
+                <Field type="text" name="password" placeholder="Password" component="input"/>
+            </div>
+            <div>
+                <Field type="checkbox" name="rememberMe" component="input"/>Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )       
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = () => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>   
 }
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import messagesReducer from './messages-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));

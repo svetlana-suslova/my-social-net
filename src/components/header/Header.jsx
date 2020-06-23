d447ef35389409fe4033ae2025701a9bab77a34d
bonial_virto <svetlana.suslova2031@gmail.com>
2020-06-23 22:28:10 +0200

    changed logo in the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from './Header.module.sass';
 import Loader from '../common/loader/Loader';
+import logo from '../../assets/img/rose.png';
 
 const Header = ({isAuth, isFetching, login, logOut}) => {
     return (
         <header className={s.header}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK__0dZPtkT7Opgx6qdPJa55dDyMpNj12UwAtzX6LjFrLatc-b&usqp=CAU" alt="logo"/>
+            <img src={logo} alt="logo"/>
             <div className={s.loginBlock}>
             { isFetching ? <Loader />: null }    
             { isAuth

0d6011c6fbe22929cff8a8b6eeca51cef6798a86
bonial_virto <svetlana.suslova2031@gmail.com>
2020-06-29 22:37:35 +0200

    changed avatar in Posts

--- src/components/profile/myPosts/post/Post.jsx
+++ src/components/profile/myPosts/post/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Post.module.sass';
+import avatar from '../../../../assets/img/avatar.png';
 
 const Post = ({message, likes}) => {
     return (
       <div className={s.item}>
-        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/PL/pl/999/EP0149-CUSA09988_00-AV00000000000001/1553560094000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000" alt="ava"/>
+        <img src={avatar} alt="ava"/>
         {message}
         <div>
           <span>{`likes: ${likes}`}</span>
